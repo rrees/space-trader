f87f092a3c2eecc7d694ba718837a0a4dae5a9da
Robert Rees <shudderfix@gmail.com>
2025-01-05 23:53:58 +0000

    Adds a Planet model

--- models.py
+++ models.py
@@ -5,11 +5,25 @@ Merchant = namedtuple("Merchant", ["credits", "ship"])
 
 
 @dataclass
+class Point:
+    x: int
+    y: int
+
+
+@dataclass
 class Ship:
     cargo_capacity: int
     jump_fuel_capacity: int
 
 
+@dataclass
+class Planet:
+    id: str
+    name: str
+    location: Point
+    tech_level: int
+
+
 Cargo = namedtuple("Cargo", ["name", "quantity"])
 
-Galaxy = namedtuple("Galaxy", ["seed"])
+Galaxy = namedtuple("Galaxy", ["seed", "size"])
